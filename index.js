abee0885d9d6c2490c48465cc0455973de41aeba
Bisrat Yalew <bisratyalew999@gmail.com>
2023-03-17 01:43:25 +0300

    Add rate limiting middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express       = require('express');
 const bodyParser    = require('body-parser');
 const path          = require('path');
+const rateLimit     = require('express-rate-limit');
 
 const router        = require('./routes');
 const app           = express();
@@ -19,9 +20,19 @@ databaseInitializer(config_app.app?.database_uri);
 
 router(app);
 
+const limiter = rateLimit({
+	windowMs: 10 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
 /* Exclude public directory */
 app.use('/public', express.static(path.join(__dirname, '/public')))
 
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
 
 /* listen for requests */
 app.listen(3000, ()=> {
